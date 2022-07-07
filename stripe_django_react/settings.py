e6084558ee9706ba8593b041ed430105654db50e
Chadha HAJJI <chadha.hadji@gmail.com>
2022-07-07 14:04:26 +0100

    deploying to Heroku

--- stripe_django_react/settings.py
+++ stripe_django_react/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / 'product'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATIC_URL = '/static/'
 MEDIA_URL ='media/'
